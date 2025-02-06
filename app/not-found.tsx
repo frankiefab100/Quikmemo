ac327a14315361b24418af09961d3c74255cd667
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-02-07 00:27:54 +0100

    fix: install eslint config for prettier and replace <a> with <Link>

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <div className="max-w-screen-xl mx-auto px-4 flex items-center justify-start h-screen md:px-8">
@@ -10,18 +12,18 @@ export default function NotFound() {
           Sorry, the page you are looking for could not be found.
         </p>
         <div className="flex flex-wrap items-center justify-center gap-3">
-          <a
+          <Link
             href="/"
             className="block py-2 px-4 text-white font-medium bg-blue-600 duration-150 hover:bg-blue-500 active:bg-blue-700 rounded-lg"
           >
             Go back
-          </a>
-          <a
-            href="#"
+          </Link>
+          <Link
+            href="/support"
             className="block py-2 px-4 text-gray-700 hover:bg-gray-50 font-medium duration-150 active:bg-gray-100 border rounded-lg"
           >
             Contact support
-          </a>
+          </Link>
         </div>
       </div>
     </div>
