1bc685176cafe5e0c87f6c39e38b0a1feba72a1f
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-03-20 21:23:47 +0100

    chore(style): Modified searchInput styling

--- components/dashboard/ui/SearchInput.tsx
+++ components/dashboard/ui/SearchInput.tsx
@@ -1,17 +1,33 @@
+"use client";
 import { Search } from "lucide-react";
-import React from "react";
+import type React from "react";
 
-const Searchbar: React.FC = () => {
+import { useState } from "react";
+
+const SearchInput = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Searching for:", searchQuery);
+  };
+
   return (
-    <div className="relative w-full max-w-md mx-2">
-      <input
-        type="text"
-        placeholder="Search note by title, content, or tags"
-        className="md:flex hidden w-full md:px-3 md:py-2 px-2 py-1 md:text-base text-sm text-gray-900 dark:text-white border rounded-md border-gray-300 dark:bg-gray-700 dark:border-gray-600 focus:outline-none focus:ring-0 placeholder-gray-400 dark:placeholder-gray-400"
-      />
-      <Search className="absolute md:right-2 md:top-2.5 w-5 h-5 -right-2 -top-2.5 text-gray-500 dark:text-gray-400" />
-    </div>
+    <form onSubmit={handleSearch} className="flex-1 max-w-md mx-4">
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <Search className="w-5 h-5 text-gray-500 dark:text-gray-400" />
+        </div>
+        <input
+          type="search"
+          className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search note by title, content, or tags..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </div>
+    </form>
   );
 };
 
-export default Searchbar;
+export default SearchInput;
