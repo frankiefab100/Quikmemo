25126ff6d05ae717f5137841c392db460c722c90
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-03-20 23:24:14 +0100

    chore: update CTA component styling

--- components/templates/CTA.tsx
+++ components/templates/CTA.tsx
@@ -2,24 +2,24 @@ import Link from "next/link";
 
 const CTA: React.FC = () => {
   return (
-    <div className="bg-[#f7f9ff] max-w-5xl mx-auto overflow-hidden rounded-xl">
-      <div className="py-16 mx-auto text-center">
-        <h3 className="text-2xl font-bold text-gray-900">
-          Try Quikmemo for free today
+    <div className="bg-[#f7f9ff] max-w-5xl mx-auto overflow-hidden rounded-xl py-16 px-8 text-center">
+      <div className="mb-8">
+        <h3 className="text-xl md:text-2xl font-bold text-gray-900">
+          Try Quikmemo for Free Today
         </h3>
         <p className="my-2 text-base font-normal text-gray-600">
           Transform your thoughts into organized notes effortlessly.
         </p>
-        <div className="mt-8">
-          <Link
-            href="/login"
-            title="Get Started for free"
-            className="px-6 py-4 font-semibold text-white  bg-blue-600 rounded-md hover:bg-blue-700 transition-all duration-200"
-          >
-            Start Writing – It&apos;s free
-          </Link>
-        </div>
       </div>
+      <Link
+        href="/register"
+        title="Get Started for free"
+        className="px-6 py-4 font-semibold text-white  bg-blue-600 rounded-md hover:bg-blue-700 transition-all duration-200"
+      >
+        Start Writing – It&apos;s free
+      </Link>
+      {/* </div> */}
+      {/* </div> */}
     </div>
   );
 };
