2b0272969de5ecd24bfec8f7559252933804b68d
frankiefab100 <ohaegbulamfrank@gmail.com>
2025-06-18 12:54:31 +0100

    fix: add /reset-password as a public route and update middleware

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,10 @@ export default auth((req) => {
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute =
+    publicRoutes.some((route) =>
+      nextUrl.pathname === route || nextUrl.pathname.startsWith(route + "/")
+    );
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   // if it is an API Next Auth route, we don't want to redirect
